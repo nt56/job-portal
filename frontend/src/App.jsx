713cb2c7d56495cca147afc6930f14d99617ed29
nt_56 <nagutirth2121@gmail.com>
2025-02-20 15:13:57 +0530

    Navbar UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/shared/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
