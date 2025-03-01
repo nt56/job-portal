22836d1986d55a9b21e8b80108971ff25654e8c4
nt_56 <nagutirth2121@gmail.com>
2025-03-01 10:11:49 +0530

    Home Page done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,17 @@
+import CategoryCarousel from "./CategoryCarousel";
+import HeroSection from "./HeroSection";
+import LatestJobs from "./LatestJobs";
+import Footer from "./shared/Footer";
 import Navbar from "./shared/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection />
+      <CategoryCarousel />
+      <LatestJobs />
+      <Footer />
     </div>
   );
 };
