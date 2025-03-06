c9f8b5a56f50de6003e0f149964431ce4bb2e4e2
nt_56 <nagutirth2121@gmail.com>
2025-03-06 14:31:48 +0530

    user profile done

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,7 +1,83 @@
 import React from "react";
+import Navbar from "./shared/Navbar";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { Contact, Mail, Pen } from "lucide-react";
+import { Label } from "./ui/label";
+import AppliedJobTable from "./AppliedJobTable";
+import { Badge } from "./ui/badge";
+
+const skills = ["html", "css", "js", "react", "node"];
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const isResume = true;
+
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
+        <div className="flex justify-between">
+          <div className="flex items-center gap-4">
+            <Avatar className="h-24 w-24">
+              <AvatarImage
+                src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"
+                alt="profile"
+              />
+            </Avatar>
+            <div>
+              <h1 className="font-medium text-xl">Full Name</h1>
+              <p>User Bio</p>
+            </div>
+          </div>
+          <Button className="text-right" variant="outline">
+            <Pen />
+          </Button>
+        </div>
+
+        <div className="my-5">
+          <div className="flex items-center gap-3 my-2">
+            <Mail />
+            <span>Email</span>
+          </div>
+          <div className="flex items-center gap-3 my-2">
+            <Contact />
+            <span>Mobile</span>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <h1>Skills</h1>
+          <div className="flex items-center gap-1">
+            {skills.length !== 0 ? (
+              skills.map((s, i) => <Badge key={i}>{s}</Badge>)
+            ) : (
+              <span>NA</span>
+            )}
+          </div>
+        </div>
+
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label className="text-md font-bold">Resume</Label>
+          {isResume ? (
+            <a
+              target="blank"
+              href="www.google.com"
+              className="text-blue-500 w-full hover:underline cursor-pointer"
+            >
+              Resume Link
+            </a>
+          ) : (
+            <span>NA</span>
+          )}
+        </div>
+      </div>
+
+      <div className="max-w-4xl mx-auto bg-white rounded-2xl">
+        <h1 className="font-bold text-lg my-5">Applied Jobs</h1>
+        <AppliedJobTable />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
